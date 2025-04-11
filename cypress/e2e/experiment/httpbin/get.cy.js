286352f48dadc1e33259df8ed89f0af30a43b069
Miguel Escandor <mescandor@vertere-gs.com>
2025-04-11 13:39:37 +0800

    Added GET request test

---
+++ cypress/e2e/experiment/httpbin/get.cy.js
@@ -0,0 +1,15 @@
+describe('GET Request - HTTPBin', () => {
+    let url = "https://httpbin.org/get"
+    before(() => {
+        cy.resetBrowser(true,true,true)
+    })
+    it('Recieves a Response code of 200', () => {
+        cy.api(url)
+        cy.request({
+            url: url
+        }).then((response) => {
+            expect(response.status).to.eql(200);
+            expect(response.headers.server).to.eql("gunicorn/19.9.0")
+        })
+    })
+})
