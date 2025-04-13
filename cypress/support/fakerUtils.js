ae2d2629176485b11063a244c19b66d750854d47
drgimatt <boxrox12@gmail.com>
2025-04-13 09:37:51 +0800

    parabank test fix

--- cypress/support/fakerUtils.js
+++ cypress/support/fakerUtils.js
@@ -2,10 +2,6 @@ import { faker } from '@faker-js/faker';
 
 export function generateCustomerData() {
 
-  const randomNumber = faker.string.numeric(2);
-  const usernameBase = faker.internet.username();
-  const username = `${usernameBase}${randomNumber}`;
-  //const username = usernameBase + randomNumber;
   return {
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
@@ -15,7 +11,7 @@ export function generateCustomerData() {
     zipCode: faker.location.zipCode(),
     phoneNum: "0000000",
     ssn: faker.string.numeric(9),
-    username: username,
-    password: 'passw0rd',
+    username: faker.internet.username(),
+    password: faker.internet.password(),
   };
 }
