8908e4d545613e5e902ee1f622366167b6d788b9
Miguel Escandor <mescandor@vertere-gs.com>
2025-04-14 15:20:50 +0800

    Changed username routine

--- cypress/support/fakerUtils.js
+++ cypress/support/fakerUtils.js
@@ -1,17 +1,22 @@
 import { faker } from '@faker-js/faker';
 
 export function generateCustomerData() {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const randomDigits = Math.floor(Math.random() * 900) + 100; // Generates a number between 100 and 999
+
+  const username = `${firstName.slice(0, 3).toLowerCase()}${lastName.slice(-3).toLowerCase()}${randomDigits}`;
 
   return {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
+    firstName: firstName,
+    lastName: lastName,
     address: faker.location.streetAddress(),
     city: faker.location.city(),
     state: faker.location.state(),
     zipCode: faker.location.zipCode(),
     phoneNum: "0000000",
     ssn: faker.string.numeric(9),
-    username: faker.internet.username(),
+    username: username,
     password: faker.internet.password(),
   };
 }
