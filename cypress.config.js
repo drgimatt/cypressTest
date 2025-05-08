5f9ec400c733b634bcb4d810a3a9404ff0e29492
Miguel Escandor <mescandor@vertere-gs.com>
2025-05-08 14:49:52 +0800

    chore(update): added back httpbin test

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,10 @@ module.exports = defineConfig({
   projectId: "eqpax6",
   experimentalStudio: true,
   env: {
-    PROJECT_NAME : 'Cypress Test Automation Training',
-    ENVIRONMENT : 'QA',
-    API_KEY : 'special-key',
-    API_BASE_URL : 'https://petstore.swagger.io/v2'
+    PROJECT_NAME: 'Cypress Test Automation Training',
+    ENVIRONMENT: 'QA',
+    API_KEY: 'special-key',
+    API_BASE_URL: 'https://petstore.swagger.io/v2'
   },
   //viewportWidth: 1920,
   //viewportHeight: 1080,
@@ -19,14 +19,14 @@ module.exports = defineConfig({
     reportPageTitle: 'custom-title',
     //reportFilename: "[status]_[datetime]-[name]-report",
     reportFilename: "code-coverage",
-    reportDir:"cypress/downloads/reports",
+    reportDir: "cypress/downloads/reports",
     //timestamp: "longDate",
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
   },
   e2e: {
-    setupNodeEvents(on, config) {  
+    setupNodeEvents(on, config) {
       cypressSplit(on, config)
       on('before:run', async (details) => {
         console.log('override before:run');
@@ -41,7 +41,7 @@ module.exports = defineConfig({
     trashAssetsBeforeRuns: true,
     excludeSpecPattern: [
       '**/affordaeats/*.cy.js',
-      '**/experiment/**/*.cy.js'
-  ],
+      '**/experiment/localhost/*.cy.js'
+    ],
   },
 });
